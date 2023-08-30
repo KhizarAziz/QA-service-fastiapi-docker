2d2f0d7ae1ac9ef9193be4985e1dafeb3a11f30d
KhizarAziz <khizar@scanbot.io>
2023-08-30 19:22:35 +0200

    reading from os

--- commons/constants.py
+++ commons/constants.py
@@ -1,6 +1,6 @@
-DB_URL = "sqlite:///commons/knowledge_base.db"
+import os
 
+DB_URL = os.environ.get('DB_URL')
 
 QA_MATCHING_SERVICE = "http://localhost:8001/match/"
 TABLE_QA = "questions_answers"
-
