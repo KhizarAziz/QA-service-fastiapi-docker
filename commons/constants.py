ccd9ac49ae30f89f8f9dcdadcb062e4f41880156
KhizarAziz <khizar@scanbot.io>
2023-08-31 06:09:19 +0200

    all services running successfully- now need to connect service 3 with 1 to store

--- commons/constants.py
+++ commons/constants.py
@@ -2,5 +2,7 @@ import os
 
 DB_URL = os.environ.get('DB_URL')
 
-QA_MATCHING_SERVICE = "http://localhost:8001/match/"
+QA_MATCHING_SERVICE = "http://matching_api:8001/match"
+QA_STORAGE_SERVICE = "http://storage_api:8002/store"
+QA_FETCH_SERVICE = "http://storage_api:8002/fetch"
 TABLE_QA = "questions_answers"
